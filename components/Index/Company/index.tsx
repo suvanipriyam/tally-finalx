520822456fdb66449a1514a57cc56119aebee377
swati <76866808+suvanipriyam@users.noreply.github.com>
2023-12-19 10:37:18 +0530

    pushed

---
+++ components/Index/Company/index.tsx
@@ -0,0 +1,43 @@
+import { FC } from 'react'
+import Accordian from '../../Accordian'
+import FooterForm from '@components/FooterForm'
+import Link from 'next/link'
+
+export const Company: FC = () => {
+  return (
+    <>
+
+
+      {/* <section
+        className="page-title parallaxie"
+        data-bg-img="/images/08.jpg"
+        style={{
+          backgroundImage: 'url("/images/08.jpg")',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundAttachment: 'fixed',
+          backgroundPosition: 'center 39.435px',
+        }}
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1>About Tally Erp9 Software</h1>
+              <nav aria-label="breadcrumb" className="page-breadcrumb">
+                <ol className="breadcrumb">
+                  <li className="breadcrumb-item">
+                    <Link href="/">Home</Link>
+                  </li>
+                  <li className="breadcrumb-item active" aria-current="page">
+                    About Tally Erp9 Softwarex
+                  </li>
+                </ol>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </section> */}
+     
+    </>
+  )
+}
